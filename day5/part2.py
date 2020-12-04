88077edf976f737fe39c9e7a3fb41147bb2207e3
David Gubitosi <david.gubitosi@gmail.com>
2020-12-04 08:11:05 -0500

    part2 solution

--- day5/part2.py
+++ day5/part2.py
@@ -1,4 +1,5 @@
 
+import re
 import sys
 
 fields = [
@@ -11,6 +12,44 @@ fields = [
   "pid", # Passport ID
   "cid", # Country ID, optional
 ]
+
+def between(v, a, b):
+    if a <= v <= b: return True
+    return False
+
+def byr(v, a=1920, b=2020):
+    return between(int(v), a, b)
+
+def iyr(v, a=2010, b=2020):
+    return between(int(v), a, b)
+
+def eyr(v, a=2020, b=2030):
+    return between(int(v), a, b)
+
+def hgt(v):
+    if v.endswith('cm'):
+        h = v.split('cm')[0]
+        return between(int(h), 150, 193)
+    if v.endswith('in'):
+        h = v.split('in')[0]
+        return between(int(h), 59, 76)
+    return False
+
+def hcl(v):
+    if re.match(r'^#[0-9a-f]{6}$', str(v)): return True
+    return False
+
+def ecl(v):
+    colors = [ "amb", "blu", "brn", "gry", "grn", "hzl", "oth" ]
+    if v in colors: return True
+    return False
+
+def pid(v):
+    if re.match(r'^[0-9]{9}$', str(v)): return True
+    return False
+
+def cid(v):
+    return True
 
 r = dict()
 records = []
@@ -32,10 +71,18 @@ valid = 0
 for r in records:
     k = r.keys()
     n = len(k)
+    is_valid = False
     if (n == 8) or (n == 7 and 'cid' not in k):
+        is_valid = True
+        for field in k:
+            validate = globals()[field]
+            if len(r[field]) != 1 or not validate(r[field][0]):
+                is_valid = False
+                break
+    if is_valid:
         valid += 1
-        print("V", k)
+        print("V", r)
     else:
-        print("*", k)
+        print("*", r)
 print(valid)
 
